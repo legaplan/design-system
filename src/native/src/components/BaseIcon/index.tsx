e42b494c81bb040660571139ef5d2803ca7eef13
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-10-26 14:48:30 -0300

    feat(icons): Add new icons

--- src/native/src/components/BaseIcon/index.tsx
+++ src/native/src/components/BaseIcon/index.tsx
@@ -6,6 +6,12 @@ import AlertCircle from "./components/AlertCircle";
 import HelpCircle from "./components/HelpCircle";
 import CheckFill from "./components/CheckFill";
 import ChevronDown from "./components/ChevronDown";
+import Doc from "./components/Doc";
+import NotificationOff from "./components/NotificationOff";
+import PaintBrush from "./components/PaintBrush";
+import UserGroup from "./components/UserGroup";
+import Home from "./components/Home";
+import Folder from "./components/Folder";
 
 const icons = {
   ["arrow-left"]: ArrowLeft,
@@ -14,6 +20,12 @@ const icons = {
   ["help-circle"]: HelpCircle,
   ["check-fill"]: CheckFill,
   ["chevron-down"]: ChevronDown,
+  ["notification-off"]: NotificationOff,
+  ["paint-brush"]: PaintBrush,
+  ["user-group"]: UserGroup,
+  home: Home,
+  folder: Folder,
+  doc: Doc,
 };
 
 export type IconKeys = keyof typeof icons;
