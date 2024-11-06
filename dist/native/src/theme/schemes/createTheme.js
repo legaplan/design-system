a3adef763e120fc4f6d39e1c8bd054d009d7cca0
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-11-06 10:12:25 -0300

    fix(#6): Fix dark and light colors

--- dist/native/src/theme/schemes/createTheme.js
+++ dist/native/src/theme/schemes/createTheme.js
@@ -9,23 +9,23 @@ const createTextScheme_1 = require("./createTextScheme");
 const createUtilityScheme_1 = require("./createUtilityScheme");
 const createDarkTheme = (theme) => {
     return {
-        components: (0, createComponentsScheme_1.createLightComponentsScheme)(theme),
-        utility: (0, createUtilityScheme_1.createLightUtilityScheme)(theme),
-        background: (0, createBackgroundScheme_1.createDarkBackgroundScheme)(theme),
-        border: (0, createBorderScheme_1.createDarkBorderScheme)(theme),
-        text: (0, createTextScheme_1.createDarkTextScheme)(theme),
-        foreground: (0, createForegroundScheme_1.createDarkForegroundScheme)(theme),
-    };
-};
-exports.createDarkTheme = createDarkTheme;
-const createLightTheme = (theme) => {
-    return {
         components: (0, createComponentsScheme_1.createDarkComponentsScheme)(theme),
         utility: (0, createUtilityScheme_1.createDarkUtilityScheme)(theme),
         background: (0, createBackgroundScheme_1.createLightBackgroundScheme)(theme),
         border: (0, createBorderScheme_1.createLightBorderScheme)(theme),
         text: (0, createTextScheme_1.createLightTextScheme)(theme),
         foreground: (0, createForegroundScheme_1.createLightForegroundScheme)(theme),
+    };
+};
+exports.createDarkTheme = createDarkTheme;
+const createLightTheme = (theme) => {
+    return {
+        components: (0, createComponentsScheme_1.createLightComponentsScheme)(theme),
+        utility: (0, createUtilityScheme_1.createLightUtilityScheme)(theme),
+        background: (0, createBackgroundScheme_1.createDarkBackgroundScheme)(theme),
+        border: (0, createBorderScheme_1.createDarkBorderScheme)(theme),
+        text: (0, createTextScheme_1.createDarkTextScheme)(theme),
+        foreground: (0, createForegroundScheme_1.createDarkForegroundScheme)(theme),
     };
 };
 exports.createLightTheme = createLightTheme;
