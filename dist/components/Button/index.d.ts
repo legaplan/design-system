ef979d7bbd95b6706abb5d3328743219bc50230b
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-11-26 16:12:47 -0300

    fix(badge): Fix badge text size

--- dist/components/Button/index.d.ts
+++ dist/components/Button/index.d.ts
@@ -9,11 +9,10 @@ export type ButtonSize = 1 | 2 | 3 | 4 | 5;
 export interface ButtonProps extends TouchableOpacityProps {
     variant?: ButtonVariant;
     size?: ButtonSize;
-    onPress: () => void;
     children: React.ReactNode;
 }
 export declare const Button: {
-    ({ variant, size, children, disabled, onPress, }: ButtonProps): import("react/jsx-runtime").JSX.Element;
+    ({ variant, size, children, disabled, onPress, ...props }: ButtonProps): import("react/jsx-runtime").JSX.Element;
     Icon: ({ name, color, size }: import("..").IconProps) => import("react/jsx-runtime").JSX.Element;
     Text: ({ children }: {
         children: React.ReactNode;
--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -4,6 +4,7 @@ import Text from "./components/Text";
 import { ComponentPropsProvider } from "../../providers/ComponentPropsProvider";
 import { Container } from "./styles";
 import { BadgeSizes, BadgeColors, BadgeVariants } from "./types";
+import { FontSizeKeys } from "@/constants/theme";
 
 export interface BadgeProps {
   size?: BadgeSizes;
@@ -18,7 +19,7 @@ export const Badge = ({
   size = 2,
   variant = "fill",
 }: BadgeProps) => {
-  const textSize = size === 1 ? "xs" : "sm";
+  const textSize: FontSizeKeys = size === 1 ? "2" : "3";
   const isOutline = variant === "outline";
   return (
     <ComponentPropsProvider value={{ color, textSize, isOutline }}>
